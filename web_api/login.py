4a21b3b62f7cb62b7157af1f5457bc5b279d0331
Ubuntu <ubuntu@ip-172-31-24-52.us-west-2.compute.internal>
2018-04-28 05:02:21 +0000

    4-27-2018

--- web_api/login.py
+++ web_api/login.py
@@ -1,5 +1,31 @@
 import constants
 import oauth2
+import urllib.parse as urlparse
 
 consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
+client = oauth2.Client(consumer)
 
+response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
+
+if response.status != 200:
+    print("An error occured")
+
+request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+# login with twitter button
+# choose "Sign In" with twitter
+# Twitter sends back to our website
+# We get auth code
+
+print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
+
+oauth_verifier = input("What is the PIN? ")
+
+token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+token.set_verifier(oauth_verifier)
+client = oauth2.Client(consumer, token)
+
+response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print(access_token)
